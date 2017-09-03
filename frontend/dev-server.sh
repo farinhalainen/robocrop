30fc02630cf967f2b991dc756e1ebdf9f0f6b55c
Sergio Garcez <garcez.sergio@gmail.com>
2017-09-04 00:08:13 +0300

    Refactor frontend

---
+++ frontend/dev-server.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+
+command -v watchman-make >/dev/null 2>&1 || {
+    echo >&2 "watchman-make is not installed. Please install with 'brew install watchman-make'. Aborting."; exit 1; }
+
+trap 'kill %1' SIGINT
+DEBUG=1 watchman-make -p '*' 'Makefile' -t all &
+cd dist; python -m SimpleHTTPServer
--- frontend/src/View.elm
+++ frontend/src/View.elm
@@ -5,13 +5,12 @@ import Types exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-import ViewUtil exposing (getRandomLeaf)
+
 
 renderPlant : Plant -> Html Msg
 renderPlant plant =
-
     li [onClick (ShowPlantView plant), listItemStyles plant.latestValue plant.threshold ]
-    [ img [src "./icons/leaf_2.svg"] []
+    [ img [src "./icons/leaf_3.svg"] []
     , div [textWrapperStyles]
         [ h3 [] [text plant.name]
         , p []  [text plant.latestReadingAt]
@@ -23,27 +22,31 @@ loaderView : Model -> Html Msg
 loaderView model =
     case model.errorMessage of
         Just message ->
-            div [ class "error" ] 
+            div [ class "error" ]
                 [ text message ]
         Nothing ->
             text "Loading..."
 
 listView : Model -> Html Msg
 listView model =
-    div [] 
-       [ ul [listStyles] (List.map renderPlant model.plants)
-       ]
+    case model.plants of
+        Just plants ->
+            div []
+                [ ul [listStyles] (List.map renderPlant plants)
+            ]
+        Nothing ->
+            text "No Plants!"
 
 detailView : Model -> Html Msg
 detailView model =
     case model.focusedPlant of
         Just plant ->
-            div [] 
+            div []
                 [ h4 [] [text plant.name]
                 , button [onClick ShowListView] [text "Go Back"]
-                ]   
+                ]
         Nothing ->
-            text "Error"
+            text "No plant selected!"
 
 view : Model -> Html Msg
 view model =
